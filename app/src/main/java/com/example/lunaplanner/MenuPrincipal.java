a353cff31af73f57f3dfe043be73737c845e8ab2
Diego Marquez <diego18976@gmail.com>
2024-08-11 23:26:36 -0600

    24. Diseño Menú Principal
    24. Diseño Menú Principal

--- app/src/main/java/com/example/lunaplanner/MenuPrincipal.java
+++ app/src/main/java/com/example/lunaplanner/MenuPrincipal.java
@@ -1,15 +1,22 @@
 package com.example.lunaplanner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import android.os.Handler;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -17,34 +24,92 @@ public class MenuPrincipal extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
 
+    TextView NombresPrincipal, CorreoPrincipal;
+    ProgressBar progressBarDatos;
+
+    DatabaseReference Usuarios;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Luna Planner");
+        }
 
+        // Inicialización de los elementos de la interfaz
+        NombresPrincipal = findViewById(R.id.NombresPrincipal);
+        CorreoPrincipal = findViewById(R.id.CorreoPrincipal);
+        progressBarDatos = findViewById(R.id.progressBarDatos);
+
+        // Referencia a la base de datos de Firebase
+        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
+
+        // Inicialización de FirebaseAuth y obtención del usuario actual
         CerrarSesion = findViewById(R.id.CerrarSesion);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
 
+        // Manejo del botón para cerrar sesión
         CerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                SalirAplicacion(v);
+            public void onClick(View view) {
+                SalirAplicacion();
             }
         });
     }
 
-    private void SalirAplicacion(View view) {
-        firebaseAuth.signOut();
-        Snackbar.make(view, "Cerraste Sesion Correctamente", Snackbar.LENGTH_LONG).show();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Verificación del inicio de sesión al iniciar la actividad
+        ComprobarInicioSesion();
+    }
 
-        // Retrasar el inicio de la nueva actividad por 3 segundos
-        new Handler().postDelayed(new Runnable() {
+    private void ComprobarInicioSesion() {
+        if (user != null) {
+            // Si el usuario ha iniciado sesión, cargar sus datos
+            CargaDeDatos();
+        } else {
+            // Si no ha iniciado sesión, redirigir a la pantalla principal
+            startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void CargaDeDatos() {
+        Usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
-                finish(); // Termina la actividad actual
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    // Ocultar el progress bar y mostrar los datos
+                    progressBarDatos.setVisibility(View.GONE);
+                    NombresPrincipal.setVisibility(View.VISIBLE);
+                    CorreoPrincipal.setVisibility(View.VISIBLE);
+
+                    // Obtener los datos de Firebase
+                    String nombres = "" + snapshot.child("nombres").getValue();
+                    String correo = "" + snapshot.child("correo").getValue();
+
+                    // Mostrar los datos en los TextViews
+                    NombresPrincipal.setText(nombres);
+                    CorreoPrincipal.setText(correo);
+                }
             }
-        }, 2000); // 3000 milisegundos = 3 segundos
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Manejar posibles errores
+            }
+        });
+    }
+
+    private void SalirAplicacion() {
+        // Cerrar la sesión y redirigir a la pantalla de inicio
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
+        finish(); // Finalizar la actividad actual
     }
 }
